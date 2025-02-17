1cfa83eb72129b32569019ee9e7bc6a0d5b06a7e
tejas <tejaszalariya@gmail.com>
2025-02-17 17:53:42 +0530

    - Merge Code

--- lib/Ui_Web/splash_screen.dart
+++ lib/Ui_Web/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fujitsuweb/Ui_web/00_Auth/login_screen.dart';
+import '../Ui_web/00_Auth/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
